e1d0976e7ee44b1758f1b48d3e72f775f1ebf056
Saksham Panwar <skshmpnwar@gmail.com>
2026-05-21 20:16:26 +0530

    Change LED pin from 13 to 8 in External_LED_Blink

--- External_LED_Blink.ino
+++ External_LED_Blink.ino
@@ -1,14 +1,11 @@
-// Define the pin where the LED is connected
-const int ledPin = 13; 
+int led = 8;
 
 void setup() {
-  // Initialize the digital pin as an output
-  pinMode(ledPin, OUTPUT);
+  pinMode(led, OUTPUT);
 }
-
 void loop() {
-  digitalWrite(ledPin, HIGH);  // Turn the LED on (HIGH is the voltage level)
-  delay(1000);                 // Wait for a second (1000 milliseconds)
-  digitalWrite(ledPin, LOW);   // Turn the LED off by making the voltage LOW
-  delay(1000);                 // Wait for a second
+  digitalWrite(led, HIGH);
+  delay(1000);
+  digitalWrite(led, LOW);
+  delay(1000);
 }
