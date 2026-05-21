1fe3215833d6ae5ba495e470784e23dc0e0c2072
Saksham Panwar <skshmpnwar@gmail.com>
2026-05-21 19:29:29 +0530

    Add LED blinking functionality

--- External_LED_Blink.ino
+++ External_LED_Blink.ino
@@ -1,1 +1,14 @@
+// Define the pin where the LED is connected
+const int ledPin = 13; 
 
+void setup() {
+  // Initialize the digital pin as an output
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH);  // Turn the LED on (HIGH is the voltage level)
+  delay(1000);                 // Wait for a second (1000 milliseconds)
+  digitalWrite(ledPin, LOW);   // Turn the LED off by making the voltage LOW
+  delay(1000);                 // Wait for a second
+}
